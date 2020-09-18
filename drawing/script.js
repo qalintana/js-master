d1be4a3066cfe074f94c1c26a0026bdb535f27c8
qalintana <alintana18@gmail.com>
2020-09-18 23:05:51 +0100

    canvas completed

--- drawing/script.js
+++ drawing/script.js
@@ -8,28 +8,37 @@ let color = 'black';
 const sizeSpan = document.getElementById('size');
 const colorEl = document.getElementById('color');
 
+const clearBtn = document.getElementById('clear');
+
 const ctx = canvas.getContext('2d');
 
 let size = 10;
-let x = 50;
-let y = 50;
+let x = undefined;
+let y = undefined;
 
 let isPressed;
 
 canvas.addEventListener('mousedown', (e) => {
   isPressed = true;
+  x = e.offsetX;
+  y = e.offsetY;
 });
 
 canvas.addEventListener('mouseup', (e) => {
   isPressed = false;
+  x = undefined;
+  y = undefined;
 });
 
 canvas.addEventListener('mousemove', (e) => {
   if (isPressed) {
-    const x = e.offsetX;
-    const y = e.offsetY;
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
 
-    drawCircle(x, y);
+    // drawCircle(x2, y2);
+    line(x, y, x2, y2);
+    x = x2;
+    y = y2;
   }
 });
 
@@ -58,20 +67,23 @@ descreaseBtn.addEventListener('click', (e) => {
   updateSizeOnScreen(size);
 });
 
+function line(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size;
+  ctx.stroke();
+}
+
 colorEl.addEventListener('change', (e) => {
   color = e.target.value;
+});
+
+clearBtn.addEventListener('click', () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
 
 function updateSizeOnScreen(size) {
   sizeSpan.textContent = size;
 }
-
-// function draw() {
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//   drawCircle(x, y);
-
-//   requestAnimationFrame(draw);
-// }
-
-// draw();
